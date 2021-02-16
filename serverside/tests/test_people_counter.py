0e8a9cbd36f3bd26f6e0824725929d7201fb73ca
yuntaowu2000 <49249544+yuntaowu2000@users.noreply.github.com>
2021-02-15 18:11:32 -0800

    fix test

--- serverside/tests/test_people_counter.py
+++ serverside/tests/test_people_counter.py
@@ -1,7 +1,7 @@
 import sys
 import os
 sys.path.append(os.getcwd())
-from modules.Person_Counter_Opencv.People_Counter import main, counter_test
+from modules.person_counter_opencv.people_counter import main, counter_test
 
 def test_module_import():
     assert counter_test() == 1
