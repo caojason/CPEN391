ff071ee593719f63b7fbc0da9a871e0bc7fe4ae7
Steven Huang <a1479839090@gmail.com>
2021-02-27 04:53:00 -0800

    updated permission request in backend

--- serverside/store_info_endpoint.py
+++ serverside/store_info_endpoint.py
@@ -1,8 +1,10 @@
 import os
 import sys
+import json
+import smtplib
 sys.path.append(os.getcwd())
 import modules.database.store_info_database as SD
-
+import modules.database.user_database.py as UD
 from app import app
 
 from flask import request, jsonify
@@ -33,3 +35,36 @@ def create_store():
 def get_store_list():
     uid = request.args["uid"] if "uid" in request.args else "\"\""
     return jsonify(SD.get_store_info_records(uid))
+
+
+@app.route("/request",method=["GET"])
+def create_permission_link():
+    data_json = request.get_json()
+    request_info=json.loads(data_json["data"])
+    email=request_info["email"]
+    message=request_info["message"]
+    ownerId=request_info["ownerId"]
+    uid=UD.get_uid(email)
+    storeInfo=SD.get_store_info_records(ownerId)
+    macAddr=storeInfo["macAddr"]
+    favourite_list_str=UD.get_favorite_list(uid)
+    favourite_list=json.loads(favourite_list_str)
+    uid=hash(uid)
+    macAddr=hash(macAddr)
+    permissionLink="/give_permission?macAddr={macAddr}&request_user_id={uid}"
+    send_email(email,"owner's email","user's password", message+"please click the link to give permission:"+permissionLink)
+    for y in favourite_list.values():   #I don't know what the list look like, so I pretend the value in the dictionary has the storeInfo 
+        if y["macAddr"] == macAddr:
+            y["permission"] == True
+        
+    toStr=str(favourite_list)
+    UD.set_favorite_list(uid, toStr)
+
+
+def send_email(from,to,message,password):
+    server=smtplib.SMTP_SSL("smtp.gmail.com",465)
+    server.login(from,password)
+    server.sendmail(from,to,message)
+    server.quit()
+
+
