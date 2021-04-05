c41c20c54f9c0b3fb9391d77551d9980ff9fc287
ShuyaoWen <wenshuyao@hotmail.com>
2021-04-04 19:58:41 -0700

    edited compression function

--- Pi/torch_pi_code.py
+++ Pi/torch_pi_code.py
@@ -42,6 +42,13 @@ def compression(img_arr):
                 #get the new color
                 color = newColor
                 count = 1
+
+    if len(compressed_arr) != 0:
+        compressed_arr.append(color[0])
+        compressed_arr.append(color[1])
+        compressed_arr.append(color[2])
+        compressed_arr.append(count)
+        
     return np.array(compressed_arr, dtype=np.uint8)
 
 try:
